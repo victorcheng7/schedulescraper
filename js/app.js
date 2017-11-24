3359a5245fac9a5fc8dd609c16808519386494d2
highiqkid <victor_cheng@yahoo.com>
2017-11-24 01:30:02 -0800

    individual page works

--- js/app.js
+++ js/app.js
@@ -3,9 +3,24 @@ var e_quarter = document.getElementById('ctl00_pageContent_quarterList');
 var e_courseLevel = document.getElementById('ctl00_pageContent_dropDownCourseLevels');
 var e_searchButton = document.getElementById('ctl00_pageContent_searchButton');
 
+/*
+var quarter = selectQuarter(0);
+var courseLevel = selectCourseLevel(0);
+var subject = selectSubjectArea(0);
+console.log(getData(subject, quarter, courseLevel));*/
+
 main();
 function main(){
   var totalCourses = [];
+
+  var quarter = selectQuarter(0);
+  var courseLevel = selectCourseLevel(0);
+  for(var i = 0; i < 2; i++){
+    var subject = selectSubjectArea(i);
+    clickSearch();
+    totalCourses.concat(getData(subject, quarter, courseLevel));
+  }
+  /*
   for(var i = 0; i < e_subjectArea.length; i++){
     var subject = selectSubjectArea(i);
     for(var j = 0; j < e_quarter.length; j++){
@@ -16,35 +31,55 @@ function main(){
         totalCourses.concat(getData(subject, quarter, courseLevel));
       }
     }
-  }
+  }*/
   console.log(totalCourses); //TODO make this less elements
+}
+
+function pause(milliseconds) {
+	var dt = new Date();
+	while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
 }
 
 function getData(subject, quarter, courseLevel){
   var coursesHTML = document.getElementsByClassName('gridview')[0].children[1].children;
   var coursesArray = [];
+
   for(var i = 0; i < coursesHTML.length; i++){
-    var masterTable = courseHTML[i];// TODO fix
+    try{
+      var basicInfo = coursesHTML[i].getElementsByClassName("Header Clickable"); //Basic Row Elements
+      basicInfo[2].click(); //Click for master table
+    }
+    catch(e){
+      continue;
+    }
+
+    var masterTable = coursesHTML[i].getElementsByClassName('MasterCourseTable')[0].querySelectorAll("tr"); // Table for extra data NOTE this may not work
+    var CourseID = basicInfo[1].innerText
+    CourseID = CourseID.substring(0, CourseID.indexOf("Click"));
     var course = {
       Subject : subject,
       Quarter: quarter,
       CourseLevel: courseLevel,
-      CourseID : ,
-      Title,
-      Full,
-      Instructor,
-      Days,
-      Time,
-      Location,
-      MaxEnrolledCount,
-      CurrentEnrolled
-      RMPscore,
-      Description,
-      Grading,
+      CourseID : CourseID,
+      Title: basicInfo[2].children[0].innerText,
+      Full: basicInfo[3].innerText,
+      Instructor: basicInfo[5].innerText,
+      Days: basicInfo[6].innerText,
+      Time: basicInfo[7].innerText,
+      Location: basicInfo[8].innerText,
+      MaxEnrolledCount: basicInfo[9].innerText.split("/")[1],
+      CurrentEnrolled: basicInfo[9].innerText.split("/")[0],
+      FullTitle: masterTable[1].children[1].innerText.trim(),
+      Description: masterTable[2].children[1].innerText.trim(),
+      PreRequisite: masterTable[3].children[1].innerText.trim(),
+      College: masterTable[4].children[1].innerText.trim(),
+      Units: masterTable[5].children[1].innerText.trim(),
+      Grading: masterTable[6].children[1].innerText.trim(),
     }
-    //TODO click on the full description AND get that data too
-    courseArray.push(course);
+    coursesArray.push(course);
+    pause(91);
   }
+
   return coursesArray;
 }
 
